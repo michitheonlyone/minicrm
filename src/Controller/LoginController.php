934f6c8e46353a19a22cf883eafdffa01517518b
Michael Ihde <michitheonlyone@gmail.com>
2022-02-05 00:24:35 +0100

    setup easyadmin with security scaffolding

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -12,12 +12,71 @@ class LoginController extends AbstractController
     #[Route('/login', name: 'login')]
     public function index(AuthenticationUtils $authenticationUtils): Response
     {
+        // https://symfony.com/bundles/EasyAdminBundle/current/dashboards.html#login-form-template
+
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('login/index.html.twig', [
+        return $this->render('@EasyAdmin/page/login.html.twig', [
+            // parameters usually defined in Symfony login forms
+            'error' => $error,
             'last_username' => $lastUsername,
-            'error'         => $error,
+
+            // OPTIONAL parameters to customize the login form:
+
+            // the translation_domain to use (define this option only if you are
+            // rendering the login template in a regular Symfony controller; when
+            // rendering it from an EasyAdmin Dashboard this is automatically set to
+            // the same domain as the rest of the Dashboard)
+            'translation_domain' => 'admin',
+
+            // the title visible above the login form (define this option only if you are
+            // rendering the login template in a regular Symfony controller; when rendering
+            // it from an EasyAdmin Dashboard this is automatically set as the Dashboard title)
+            'page_title' => '<h1>'.$this->getParameter('app.name').' | login</h1>',
+
+            // the string used to generate the CSRF token. If you don't define
+            // this parameter, the login form won't include a CSRF token
+            'csrf_token_intention' => 'authenticate',
+
+            // the URL users are redirected to after the login (default: '/admin')
+            'target_path' => $this->generateUrl('home'),
+
+            // the label displayed for the username form field (the |trans filter is applied to it)
+            'username_label' => 'E-Mail',
+
+            // the label displayed for the password form field (the |trans filter is applied to it)
+            'password_label' => 'Password',
+
+            // the label displayed for the Sign In form button (the |trans filter is applied to it)
+            'sign_in_label' => 'Log in',
+
+            // the 'name' HTML attribute of the <input> used for the username field (default: '_username')
+            'username_parameter' => '_username',
+
+            // the 'name' HTML attribute of the <input> used for the password field (default: '_password')
+            'password_parameter' => '_password',
+
+            // whether to enable or not the "forgot password?" link (default: false)
+            'forgot_password_enabled' => false,
+
+            // the path (i.e. a relative or absolute URL) to visit when clicking the "forgot password?" link (default: '#')
+//            'forgot_password_path' => $this->generateUrl('...', ['...' => '...']),
+
+            // the label displayed for the "forgot password?" link (the |trans filter is applied to it)
+            'forgot_password_label' => 'Forgot your password?',
+
+            // whether to enable or not the "remember me" checkbox (default: false)
+            'remember_me_enabled' => true,
+
+            // remember me name form field (default: '_remember_me')
+            'remember_me_parameter' => '_remember_me',
+
+            // whether to check by default the "remember me" checkbox (default: false)
+            'remember_me_checked' => true,
+
+            // the label displayed for the remember me checkbox (the |trans filter is applied to it)
+            'remember_me_label' => 'Remember me',
         ]);
     }
 }
